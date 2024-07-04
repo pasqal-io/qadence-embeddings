0a7fbea8dc60fc758d4cfdc1f296909fc593b789
seitzdom <dominik.seitz@pasqal.com>
2024-07-04 13:31:13 +0200

    more tests

--- tests/test_callable.py
+++ tests/test_callable.py
@@ -18,6 +18,18 @@ def test_sin() -> None:
     assert np.allclose(results[0], results[1]) and np.allclose(results[0], results[2])
 
 
+def test_cos() -> None:
+    results = []
+    x = np.random.randn(1)
+    for engine_name in ["jax", "torch", "numpy"]:
+        native_call = ConcretizedCallable("cos", ["x"], {}, engine_name)
+        native_result = native_call(
+            {"x": (torch.tensor(x) if engine_name == "torch" else x)}
+        )
+        results.append(native_result.item())
+    assert np.allclose(results[0], results[1]) and np.allclose(results[0], results[2])
+
+
 def test_log() -> None:
     results = []
     x = np.random.uniform(0, 5)
