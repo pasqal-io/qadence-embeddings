ff826b9d474efccc37bb63b2e338dbc5d99b33d6
seitzdom <dominik.seitz@pasqal.com>
2024-07-05 08:52:06 +0200

    only alloc once

--- qadence_embeddings/embedding.py
+++ qadence_embeddings/embedding.py
@@ -50,6 +50,7 @@ class Embedding:
         self._dtype: DTypeLike = None
         self.time_dependent_vars: list[str] = []
         self._device = device
+        self._time_vars_identified = False
 
     @property
     def root_param_names(self) -> list[str]:
@@ -65,16 +66,19 @@ class Embedding:
         """
         for intermediate_or_leaf_var, engine_callable in self.var_to_call.items():
             # We mutate the original inputs dict and include intermediates and leaves.
-            if self.tparam_name and any(
-                [
-                    p in [self.tparam_name] + self.time_dependent_vars
-                    for p in engine_callable.abstract_args
-                ]  # we check if any parameter in the callables args is time
-                # or depends on an intermediate variable which itself depends on time
-            ):
-                self.time_dependent_vars.append(intermediate_or_leaf_var)
-                # we remember which parameters depend on time
+            if not self._time_vars_identified:
+                # we do this only on the first embedding call
+                if self.tparam_name and any(
+                    [
+                        p in [self.tparam_name] + self.time_dependent_vars
+                        for p in engine_callable.abstract_args
+                    ]  # we check if any parameter in the callables args is time
+                    # or depends on an intermediate variable which itself depends on time
+                ):
+                    self.time_dependent_vars.append(intermediate_or_leaf_var)
+                    # we remember which parameters depend on time
             inputs[intermediate_or_leaf_var] = engine_callable(inputs)
+        self._time_vars_identified = True
         return inputs
 
     def reembed_time(
